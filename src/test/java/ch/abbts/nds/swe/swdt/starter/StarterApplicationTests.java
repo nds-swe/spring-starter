ef2815a8732d156eda7a45cab21cd44c50797f24
dom <dominik.meyer01@sap.com>
2021-04-03 21:04:43 +0200

    Add Starter App

---
+++ src/test/java/ch/abbts/nds/swe/swdt/starter/StarterApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.abbts.nds.swe.swdt.starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

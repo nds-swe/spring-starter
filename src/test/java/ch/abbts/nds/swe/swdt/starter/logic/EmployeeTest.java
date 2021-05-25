bf46a9b760ee9e643d9278ef316b2215967c7042
dom <dominik.meyer01@sap.com>
2021-05-25 21:06:52 +0200

    FIXED IT, check it teacher Pasha

--- src/test/java/ch/abbts/nds/swe/swdt/starter/logic/EmployeeTest.java
+++ src/test/java/ch/abbts/nds/swe/swdt/starter/logic/EmployeeTest.java
@@ -1,7 +1,9 @@
 package ch.abbts.nds.swe.swdt.starter.logic;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -9,6 +11,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class EmployeeTest {
   @Test
   @DisplayName("Is too young")
+  @Tag("unit")
   public void tooYoung() {
     Employee employee = new Employee("A", 12);
     assertFalse(employee.isOfAge(22), "Too young returns false");
@@ -16,6 +19,7 @@ public class EmployeeTest {
 
   @Test
   @DisplayName("Old enough")
+  @Tag("unit")
   public void oldEnough() {
     Employee employee = new Employee("A",24);
     assertTrue(employee.isOfAge(22), "Old enough returns true");
@@ -23,6 +27,7 @@ public class EmployeeTest {
 
   @Test
   @DisplayName("Of exact age")
+  @Tag("unit")
   public void exactAge() {
     Employee employee = new Employee("A",24);
     assertTrue(employee.isOfAge(24), "Exact age must also return true");

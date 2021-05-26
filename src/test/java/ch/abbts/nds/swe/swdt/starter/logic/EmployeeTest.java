d32e98645cc0e63e64cf0e87db6e652ee6f87212
dom <dominik.meyer01@sap.com>
2021-05-26 18:46:31 +0200

    Apply feedback pazone

--- src/test/java/ch/abbts/nds/swe/swdt/starter/logic/EmployeeTest.java
+++ src/test/java/ch/abbts/nds/swe/swdt/starter/logic/EmployeeTest.java
@@ -8,10 +8,10 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
+@Tag("unit")
 public class EmployeeTest {
   @Test
   @DisplayName("Is too young")
-  @Tag("unit")
   public void tooYoung() {
     Employee employee = new Employee("A", 12);
     assertFalse(employee.isOfAge(22), "Too young returns false");
@@ -19,17 +19,15 @@ public class EmployeeTest {
 
   @Test
   @DisplayName("Old enough")
-  @Tag("unit")
   public void oldEnough() {
-    Employee employee = new Employee("A",24);
+    Employee employee = new Employee("A", 24);
     assertTrue(employee.isOfAge(22), "Old enough returns true");
   }
 
   @Test
   @DisplayName("Of exact age")
-  @Tag("unit")
   public void exactAge() {
-    Employee employee = new Employee("A",24);
+    Employee employee = new Employee("A", 24);
     assertTrue(employee.isOfAge(24), "Exact age must also return true");
   }
 }

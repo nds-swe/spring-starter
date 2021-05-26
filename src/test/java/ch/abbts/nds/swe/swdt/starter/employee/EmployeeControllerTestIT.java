5ef7236b2d2f7468b179250c0fe49fca6f7d7294
dom <dominik.meyer01@sap.com>
2021-05-26 18:46:31 +0200

    Apply feedback pazone

--- src/test/java/ch/abbts/nds/swe/swdt/starter/employee/EmployeeControllerTestIT.java
+++ src/test/java/ch/abbts/nds/swe/swdt/starter/employee/EmployeeControllerTestIT.java
@@ -1,14 +1,16 @@
 package ch.abbts.nds.swe.swdt.starter.employee;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.is;
 
+@Tag("IT")
 public class EmployeeControllerTestIT {
   @Test
-  @Tag("IT")
+  @DisplayName("/employees/ returns 200 and a 3 employees")
   public void
   employee_resource_returns_200_with_expected_amount_of_employees() {
     given().
